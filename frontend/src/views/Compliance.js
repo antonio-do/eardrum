10cc0cce1f4a08182eb434cfea0804927942d07d
Hugh Do <mhughdo@gmail.com>
2021-02-23 22:07:30 +0700

    issue #2: refactor formA

--- frontend/src/views/Compliance.js
+++ frontend/src/views/Compliance.js
@@ -4,37 +4,306 @@ import {Popconfirm, Table, Space, Menu, Dropdown, Button, Breadcrumb, message, T
 import {DownOutlined, MenuOutlined} from '@ant-design/icons'
 import {Link, useRouteMatch, useParams, useHistory} from 'react-router-dom'
 import axios from 'axios'
+import routes from '../routes'
 import messages from '../messages'
 
 const {TabPane} = Tabs
 
 const typeToFormProps = messages.compliance
+const complianceRoutes = routes.compliance
+
+const FormAList = ({formName, onRowDelete, isLoading, data}) => {
+  const {url} = useRouteMatch()
+
+  const columns = [
+    {
+      title: 'Form',
+      dataIndex: 'id',
+      key: 'id',
+      render: (text, record) => {
+        const form = typeToFormProps[record.typ]
+        return <Link to={`${url}/${record.id}/view`}>{form.name}</Link>
+      },
+    },
+    {
+      title: 'Period',
+      render: (text, record) => {
+        const data = record.json_data
+        return data.submissionDate
+      },
+    },
+    {
+      title: 'Submit By',
+      dataIndex: 'submit_by',
+      key: 'submit_by',
+    },
+    {
+      title: 'Status',
+      render: () => {
+        return 'Complete'
+      },
+    },
+    {
+      title: 'Action',
+      key: 'action',
+      render: (text, record) => {
+        return (
+          <Space size='middle'>
+            <Link to={`${url}/${record.id}/edit`}>Edit</Link>
+            <Popconfirm
+              title='Are you sure to delete this form?'
+              onConfirm={onRowDelete(record.id)}
+              okText='Yes'
+              cancelText='No'>
+              <Button type='link' danger>
+                Delete
+              </Button>
+            </Popconfirm>
+          </Space>
+        )
+      },
+    },
+  ]
+
+  return (
+    <>
+      <Breadcrumb style={{marginBottom: '10px'}}>
+        <Breadcrumb.Item>
+          <MenuOutlined /> {formName} Form List
+        </Breadcrumb.Item>
+      </Breadcrumb>
+      <Table loading={isLoading} rowKey={(record) => record.id} columns={columns} dataSource={data} />
+    </>
+  )
+}
+
+const FormBList = ({formName, onRowDelete, isLoading, data}) => {
+  const {url} = useRouteMatch()
+
+  const columns = [
+    {
+      title: 'Form',
+      dataIndex: 'id',
+      key: 'id',
+      render: (text, record) => {
+        const form = typeToFormProps[record.typ]
+        return <Link to={`${url}/${record.id}/view`}>{form.name}</Link>
+      },
+    },
+    {
+      title: 'Period',
+      render: (text, record) => {
+        const data = record.json_data
+        return data.year
+      },
+    },
+    {
+      title: 'Submit By',
+      dataIndex: 'submit_by',
+      key: 'submit_by',
+    },
+    {
+      title: 'Status',
+      render: () => {
+        return 'Complete'
+      },
+    },
+    {
+      title: 'Action',
+      key: 'action',
+      render: (text, record) => {
+        return (
+          <Space size='middle'>
+            <Link to={`${url}/${record.id}/edit`}>Edit</Link>
+            <Popconfirm
+              title='Are you sure to delete this form?'
+              onConfirm={onRowDelete(record.id)}
+              okText='Yes'
+              cancelText='No'>
+              <Button type='link' danger>
+                Delete
+              </Button>
+            </Popconfirm>
+          </Space>
+        )
+      },
+    },
+  ]
+
+  return (
+    <>
+      <Breadcrumb style={{marginBottom: '10px'}}>
+        <Breadcrumb.Item>
+          <MenuOutlined /> {formName} Form List
+        </Breadcrumb.Item>
+      </Breadcrumb>
+      <Table loading={isLoading} rowKey={(record) => record.id} columns={columns} dataSource={data} />
+    </>
+  )
+}
+
+const FormCList = ({formName, onRowDelete, isLoading, data}) => {
+  const {url} = useRouteMatch()
+
+  const columns = [
+    {
+      title: 'Form',
+      dataIndex: 'id',
+      key: 'id',
+      render: (text, record) => {
+        const form = typeToFormProps[record.typ]
+        return <Link to={`${url}/${record.id}/view`}>{form.name}</Link>
+      },
+    },
+    {
+      title: 'Period',
+      render: (text, record) => {
+        const data = record.json_data
+        return `${data.quarter}/${data.year}`
+      },
+    },
+    {
+      title: 'Submit By',
+      dataIndex: 'submit_by',
+      key: 'submit_by',
+    },
+    {
+      title: 'Status',
+      render: () => {
+        return 'Complete'
+      },
+    },
+    {
+      title: 'Action',
+      key: 'action',
+      render: (text, record) => {
+        return (
+          <Space size='middle'>
+            <Link to={`${url}/${record.id}/edit`}>Edit</Link>
+            <Popconfirm
+              title='Are you sure to delete this form?'
+              onConfirm={onRowDelete(record.id)}
+              okText='Yes'
+              cancelText='No'>
+              <Button type='link' danger>
+                Delete
+              </Button>
+            </Popconfirm>
+          </Space>
+        )
+      },
+    },
+  ]
+
+  return (
+    <>
+      <Breadcrumb style={{marginBottom: '10px'}}>
+        <Breadcrumb.Item>
+          <MenuOutlined /> {formName} Form List
+        </Breadcrumb.Item>
+      </Breadcrumb>
+      <Table loading={isLoading} rowKey={(record) => record.id} columns={columns} dataSource={data} />
+    </>
+  )
+}
+
+const FormDList = ({formName, onRowDelete, isLoading, data}) => {
+  const {url} = useRouteMatch()
+
+  const columns = [
+    {
+      title: 'Form',
+      dataIndex: 'id',
+      key: 'id',
+      render: (text, record) => {
+        const form = typeToFormProps[record.typ]
+        return <Link to={`${url}/${record.id}/view`}>{form.name}</Link>
+      },
+    },
+    {
+      title: 'Period',
+      render: (text, record) => {
+        const data = record.json_data
+        return data.submissionDate
+      },
+    },
+    {
+      title: 'Submit By',
+      dataIndex: 'submit_by',
+      key: 'submit_by',
+    },
+    {
+      title: 'Status',
+      render: () => {
+        return 'Incomplete'
+      },
+    },
+    {
+      title: 'Action',
+      key: 'action',
+      render: (text, record) => {
+        return (
+          <Space size='middle'>
+            <Link to={`${url}/${record.id}/edit`}>Edit</Link>
+            <Popconfirm
+              title='Are you sure to delete this form?'
+              onConfirm={onRowDelete(record.id)}
+              okText='Yes'
+              cancelText='No'>
+              <Button type='link' danger>
+                Delete
+              </Button>
+            </Popconfirm>
+          </Space>
+        )
+      },
+    },
+  ]
+
+  return (
+    <>
+      <Breadcrumb style={{marginBottom: '10px'}}>
+        <Breadcrumb.Item>
+          <MenuOutlined /> {formName} Form List
+        </Breadcrumb.Item>
+      </Breadcrumb>
+      <Table loading={isLoading} rowKey={(record) => record.id} columns={columns} dataSource={data} />
+    </>
+  )
+}
 
 const ComplianceApp = () => {
   const {url} = useRouteMatch()
   const [formList, setFormList] = useState([])
   const [isLoading, setIsLoading] = useState(false)
   const {formType} = useParams()
-  const [tabKey, setTabKey] = useState(() => (formType in typeToFormProps ? formType : 'a'))
+  const [tabKey, setTabKey] = useState(() => (formType in typeToFormProps ? formType : Object.keys(typeToFormProps)[0]))
   const history = useHistory()
+  const typeToFormComponent = {
+    a: FormAList,
+    b: FormBList,
+    c: FormCList,
+    d: FormDList,
+  }
 
   useEffect(() => {
     setIsLoading(true)
 
     if (!formType || !(formType in typeToFormProps)) {
-      history.replace('/compliance/a')
+      history.replace(`/compliance/${Object.keys(typeToFormProps)[0]}`)
     }
 
     axios
-      .get('/api/compliance/')
+      .get(complianceRoutes.list())
       .then(({data}) => {
         setFormList(data)
-        setIsLoading(false)
       })
       .catch((err) => {
         console.log(err)
-        setIsLoading(false)
         message.error('Unexpected error encountered')
+      })
+      .finally(() => {
+        setIsLoading(false)
       })
   }, [])
 
@@ -45,7 +314,7 @@ const ComplianceApp = () => {
 
   const onDelete = (formId) => async () => {
     try {
-      const res = await axios.delete(`/api/compliance/${formId}/`)
+      const res = await axios.delete(complianceRoutes.detailsURL(formId))
 
       if (res.status === 204) {
         setFormList((formList) => formList.filter((form) => form.id !== formId))
@@ -71,78 +340,6 @@ const ComplianceApp = () => {
     </Menu>
   )
 
-  const columns = [
-    {
-      title: 'Form',
-      dataIndex: 'id',
-      key: 'id',
-      render: (text, record) => {
-        const form = typeToFormProps[record.typ]
-        return <Link to={`${url}/${record.id}/view`}>{form.name}</Link>
-      },
-    },
-    {
-      title: 'Period',
-      render: (text, record) => {
-        const data = record.json_data
-
-        switch (record.typ) {
-          case 'a':
-          case 'd':
-            return data.submissionDate
-          case 'b':
-            return data.year
-          case 'c':
-            return `${data.quarter}/${data.year}`
-          default:
-            return ''
-        }
-      },
-    },
-    {
-      title: 'Submit By',
-      dataIndex: 'submit_by',
-      key: 'submit_by',
-    },
-    {
-      title: 'Status',
-      render: (text, record) => {
-        switch (record.typ) {
-          case 'a':
-          case 'c':
-          case 'b':
-            return 'Complete'
-          case 'd':
-            return 'Incomplete'
-          default:
-            return ''
-        }
-      },
-    },
-    {
-      title: 'Action',
-      key: 'action',
-      render: (text, record) => {
-        const form = typeToFormProps[record.typ]
-
-        return (
-          <Space size='middle'>
-            <Link to={`${url}/${record.id}/edit`}>Edit</Link>
-            <Popconfirm
-              title='Are you sure to delete this form?'
-              onConfirm={onDelete(record.id)}
-              okText='Yes'
-              cancelText='No'>
-              <Button type='link' danger>
-                Delete
-              </Button>
-            </Popconfirm>
-          </Space>
-        )
-      },
-    },
-  ]
-
   return (
     <div
       style={{
@@ -161,20 +358,11 @@ const ComplianceApp = () => {
       <Tabs onChange={onTabKeyChange} type='card' activeKey={tabKey}>
         {Object.entries(typeToFormProps).map(([type, form]) => {
           const specificFormList = formList.filter((f) => f.typ === type)
+          const FormList = typeToFormComponent[type]
 
           return (
             <TabPane tab={form.name} key={type}>
-              <Breadcrumb style={{marginBottom: '10px'}}>
-                <Breadcrumb.Item>
-                  <MenuOutlined /> {typeToFormProps[tabKey].name} Form List
-                </Breadcrumb.Item>
-              </Breadcrumb>
-              <Table
-                loading={isLoading}
-                rowKey={(record) => record.id}
-                columns={columns}
-                dataSource={specificFormList}
-              />
+              <FormList formName={form.name} onRowDelete={onDelete} data={specificFormList} isLoading={isLoading} />
             </TabPane>
           )
         })}
