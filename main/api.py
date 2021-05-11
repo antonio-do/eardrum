f4a1b37de61cf6589f4deeb2cbedd0c6810808b2
Hugh Do <mhughdo@gmail.com>
2021-05-11 13:45:12 +0700

    issue #28: create api for guideline

---
+++ main/api.py
@@ -0,0 +1,19 @@
+from rest_framework import (
+    viewsets,
+    permissions,
+    status,
+    response,
+)
+
+from .models import Guideline
+from .serializers import GuidelineSerializer
+
+
+class GuidelineViewSet(viewsets.ModelViewSet):
+    permission_classes = [permissions.AllowAny]
+    queryset = Guideline.objects.all()
+    serializer_class = GuidelineSerializer
+
+    def get_queryset(self):
+        return self.queryset.all()
+
---
+++ main/serializers.py
@@ -0,0 +1,11 @@
+import json
+
+from rest_framework import serializers
+
+from .models import Guideline
+
+
+class GuidelineSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Guideline
+        fields = '__all__'
