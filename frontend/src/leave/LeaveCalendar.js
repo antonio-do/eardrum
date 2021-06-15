fab5613752534ae6552f93027ffc5310106c8ae1
PhongTran98 <giaphong8124@gmail.com>
2021-06-15 18:45:35 +0800

    Issue #33: use chip for on leave user

--- frontend/src/leave/LeaveCalendar.js
+++ frontend/src/leave/LeaveCalendar.js
@@ -2,7 +2,8 @@ import React, { Fragment, useEffect, useState } from "react";
 import { DatePicker } from "@material-ui/pickers";
 import { MuiPickersUtilsProvider } from '@material-ui/pickers';
 import DateFnsUtils from '@date-io/date-fns';
-import { Divider, List, ListItem, ListItemText, ListSubheader, makeStyles, Paper } from "@material-ui/core";
+import { Card, CardActions, CardContent, CardHeader, Chip, Dialog, DialogTitle, Divider, IconButton, List, ListItem, ListItemText, ListSubheader, makeStyles, Paper, TextField } from "@material-ui/core";
+import MoreHoriz from '@material-ui/icons/MoreHoriz';
 
 const useStyles = makeStyles(theme => ({
     root: {
@@ -11,38 +12,61 @@ const useStyles = makeStyles(theme => ({
     list: {
         backgroundColor: theme.palette.primary.main,
         color: "white",
+    },
+    yearInput: {
+        padding: theme.spacing(2)
+    },
+    chips: {
+        margin: theme.spacing(0.5)
     }
 }))
 
 const StaticDatePicker = () => {
   const [date, setDate] = useState(new Date());
   const [openDialog, setOpenDialog] = useState(false);
+  const [group, setGroup] = useState("");
   const [holidays, setHolidays] = useState([]);
   const [leaveUsers, setLeaveUsers] = useState([])
+  const [year, setYear] = useState(new Date().getFullYear());
 
   const classes = useStyles();
 
   const dateFns = new DateFnsUtils();
 
   useEffect(() => {
-      getHolidays();
+      getHolidays(year);
       handleChangeDate(new Date());
   }, [])
 
-  const getHolidays = () => {
+  const getHolidays = (year) => {
     //TODO: replace mock data
     setHolidays([
-        { "id": 1, "date": new Date(2021, 5, 14), "title": "some name" },
-        { "id": 2, "date": new Date(2021, 5, 15), "title": "some name 2" },
-        { "id": 3, "date": new Date(2021, 5, 16), "title": "some name 3" },
-        { "id": 4, "date": new Date(2021, 5, 17), "title": "some name 4" },
+        { "id": 1, "date": new Date(year, 5, 14), "title": "some name" },
+        { "id": 2, "date": new Date(year, 5, 15), "title": "some name 2" },
+        { "id": 3, "date": new Date(year, 5, 16), "title": "some name 3" },
+        { "id": 4, "date": new Date(year, 5, 17), "title": "some name 4" },
     ])
   }
 
   const handleChangeDate = (date) => {
     setDate(date);
     // TODO: set users on leave on that day
-    setLeaveUsers(["Alice", "Bob", "Mallory"]);
+    setLeaveUsers([{
+        group: "Group 1",
+        users: ["Alice", "Bob", "Mallory", "Valentine", "Maria", "Lord"],
+    }, { 
+        group: "Group 2",
+        users: ["Alice", "Bob", "Mallory", "Valentine", "Maria", "Lord"],
+    }, { 
+        group: "Group 3",
+        users: ["Alice", "Bob", "Mallory", "Valentine", "Maria", "Lord"],
+    }, { 
+        group: "Group 4",
+        users: ["Alice", "Bob", "Mallory", "Valentine", "Maria", "Lord"],
+    }, { 
+        group: "Group 5",
+        users: ["Alice", "Bob", "Mallory", "Valentine", "Maria", "Lord"],
+    }, ]);
   }
 
   const renderDay=(day, selectedDate, dayInCurrentMonth, dayComponent) => { 
@@ -57,6 +81,10 @@ const StaticDatePicker = () => {
     }
     return dayComponent;
   };
+
+  const handleClickChip = (group) => {
+      
+  }
 
   return (
     <Paper className={classes.root}>
@@ -77,20 +105,49 @@ const StaticDatePicker = () => {
         <Divider/>
         {/* The list of user on leave */}
         <ListSubheader className={classes.list}>Users on leave</ListSubheader>
-        <Paper style={{maxHeight: 200, overflow: 'auto'}}>
+        <Paper style={{overflow: 'auto'}}>
             <List>
-                {leaveUsers.map(user => 
-                    (<Fragment>
-                        <ListItem>
-                            <ListItemText primary={user} />
-                        </ListItem>
+                {leaveUsers.map(item => (
+                    <Fragment>
+                        <Card style={{display: 'flex', flexWrap: 'wrap', maxWidth: '310px'}}>
+                            <CardContent style={{padding: 5}}>
+                                <Fragment>
+                                    <div className={classes.chips}>{item.group}</div>
+                                    {item.users.map(user => 
+                                        (<Chip label={user} className={classes.chips}/>)
+                                    )}
+                                    <Chip onClick={() => handleClickChip(item.group)} label="More..." variant="outlined" color="primary" className={classes.chips}/>
+                                </Fragment>
+                            </CardContent>
+                        </Card>
                         <Divider/>
-                    </Fragment>)
-                )}
+                    </Fragment>
+                ))}
             </List>
         </Paper>
+        <Dialog open={openDialog} onClose={setOpenDialog}>
+            <Fragment>
+                {/* <div className={classes.chips}>{group}</div>
+                {item.users.map(user => 
+                    (<Chip label={user} className={classes.chips}/>)
+                )}
+                <Chip onClick={() => handleClickChip(item.group)} label="More..." variant="outlined" color="primary" className={classes.chips}/> */}
+            </Fragment>
+        </Dialog>
         {/* The list of holidays */}
         <ListSubheader className={classes.list}>Holidays</ListSubheader>
+        <div className={classes.yearInput}>
+            <TextField 
+                label="Year" 
+                type="number" 
+                defaultValue={year} 
+                fullWidth
+                onChange={(event) => {
+                    setYear(event.target.value);
+                    getHolidays(event.target.value);
+                }}
+            />
+        </div>
         <Paper>
             <List>
                 {holidays.map(item => 
