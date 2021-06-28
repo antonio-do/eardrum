13a7d4e15ff66ca983de6e10dd10dbb187e59b0b
PhongTran98 <giaphong8124@gmail.com>
2021-06-28 18:20:21 +0800

    Issue #29: Complete validations for leave application fields

--- leave/serializers.py
+++ leave/serializers.py
@@ -1,8 +1,11 @@
 from rest_framework import serializers
 from rest_framework.fields import empty
 
-from .models import Leave
+from .models import Leave, ConfigEntry
+from django.contrib.auth.models import User
 
+import json
+import datetime
 
 class LeaveSerializer(serializers.ModelSerializer):
     HALF_CHOICES = '00,01,10,11'.split(',')
@@ -10,14 +13,21 @@ class LeaveSerializer(serializers.ModelSerializer):
     def __init__(self, instance=None, data=empty, **kwargs):
         self.fields['half'] = serializers.ChoiceField(choices=self.HALF_CHOICES)
 
+        # query from context
         # TODO get typ choices from database ConfigEntry(name='leave_limitation')
         # typ = serializers.ChoiceField(choices=)
+        self.fields['typ'] = serializers.ChoiceField(
+            choices=[item['name'] for item in json.loads(ConfigEntry.objects.get(name="leave_context").extra)['leave_types']])
 
         # TODO get user choices from database Users.objects.all()
         # user = serializers.ChoiceField(choices=)
+        self.fields['user'] = serializers.ChoiceField(
+            choices=[user.username for user in User.objects.all()])
 
         # TODO get status choices from database ConfigEntry(name='leave_status_choices')
         # status = serializers.ChoiceField(choices=)
+        self.fields['status'] = serializers.ChoiceField(
+            choices=[item['name'] for item in json.loads(ConfigEntry.objects.get(name="leave_context").extra)['leave_statuses']])
 
         super().__init__(instance, data, **kwargs)
 
@@ -31,6 +41,11 @@ class LeaveSerializer(serializers.ModelSerializer):
         startdate should have format of YYYYMMDD
         """
         # TODO startdate should have format of YYYYMMDD
+        try:
+            datetime.datetime.strptime(value, '%Y%m%d')
+        except ValueError:
+            raise serializers.ValidationError("Incorrect data format, should be YYYYMMDD")
+        
         return value
 
     def validate_enddate(self, value):
@@ -39,4 +54,9 @@ class LeaveSerializer(serializers.ModelSerializer):
         enddate should have format of YYYYMMDD
         """
         # TODO enddate should have format of YYYYMMDD
+        try:
+            datetime.datetime.strptime(value, '%Y%m%d')
+        except ValueError:
+            raise serializers.ValidationError("Incorrect data format, should be YYYYMMDD")
+
         return value
