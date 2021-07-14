f8f259c365dc308fc8f27c8a6c77423bb3a5b713
PhongTran98 <giaphong8124@gmail.com>
2021-07-14 17:11:06 +0800

    Issue #35: Add api for adding and deleting holidays

--- leave/api.py
+++ leave/api.py
@@ -41,6 +41,13 @@ class LeaveViewSet(mixins.CreateModelMixin,
         return permissions.IsAdminUser.has_permission(None, self.request, self)
 
     def get_validated_query_value(self, fieldname, value):
+        def validate_date(value):
+            try: 
+                date = datetime.datetime.strptime(value, '%Y%m%d')
+                return value
+            except (ValueError, TypeError):
+                return None
+                
         def validate_year(value):
             try:
                 year = int(value)
@@ -63,6 +70,7 @@ class LeaveViewSet(mixins.CreateModelMixin,
         validation_funcs = {
             'year': validate_year,
             'status': validate_status,
+            'date': validate_date,
         }
 
         return (fieldname, validation_funcs[fieldname](value)) if validation_funcs.get(fieldname) is not None else (fieldname, None)
@@ -163,20 +171,73 @@ class LeaveViewSet(mixins.CreateModelMixin,
 
         return Response(res)
 
-    @decorators.action(methods=['GET'], detail=False)
-    def holidays(self, request, *args, **kargs):
-        year = request.query_params.get('year')
-        _, year = self.get_validated_query_value('year', year)
-        if year is not None:
-            try:
-                config_entry = ConfigEntry.objects.get(name='holidays_{}'.format(year))
-            except ConfigEntry.DoesNotExist:
-                return Response(None, status=status.HTTP_404_NOT_FOUND)
+    @decorators.action(methods=['GET', 'POST', 'DELETE'], detail=False)
+    def holidays(self, request, *args, **kargs):    
+        if request.method == "GET":
+            year = request.query_params.get('year')
+            _, year = self.get_validated_query_value('year', year)
+            if year is not None:
+                try:
+                    config_entry = ConfigEntry.objects.get(name='holidays_{}'.format(year))
+                except ConfigEntry.DoesNotExist:
+                    return Response(None, status=status.HTTP_404_NOT_FOUND)
+                else:
+                    holidays = config_entry.extra.split()
+                    return Response(holidays)
             else:
-                holidays = config_entry.extra.split()
-                return Response(holidays)
-        else:
-            return Response(None, status=status.HTTP_404_NOT_FOUND)
+                return Response(None, status=status.HTTP_404_NOT_FOUND)
+
+        elif request.method == "POST":
+            date = request.data.get('date')
+            _, date = self.get_validated_query_value('date', date)
+            if date is not None:
+                year = date[:4]
+
+                (config_entry, create) = ConfigEntry.objects.get_or_create(name='holidays_{}'.format(year))
+                if config_entry.extra is None:
+                    config_entry.extra = ""
+                if date not in config_entry.extra:
+                    config_entry.extra = '\n'.join(config_entry.extra.split() + [date])
+                    config_entry.save(update_fields=['extra'])
+                
+                return Response({'date': date}, status=status.HTTP_201_CREATED)
+            else:
+                ret = {
+                    "message": "date not provided or not in the correct format (YYYYMMDD)"
+                }
+
+                return Response(ret, status=status.HTTP_400_BAD_REQUEST)
+
+        elif request.method == "DELETE":
+            date = request.query_params.get('date')
+            _, date = self.get_validated_query_value('date', date)
+            if date is not None:
+                year = date[:4]
+
+                try: 
+                    config_entry = ConfigEntry.objects.get(name='holidays_{}'.format(year))
+                
+                except ConfigEntry.DoesNotExist:
+                    return Response(status=status.HTTP_404_NOT_FOUND)
+                
+                else:
+                    if config_entry.extra is None:
+                        config_entry.extra = ""
+
+                    if date in config_entry.extra:
+                        config_entry.extra = '\n'.join([holiday for holiday in config_entry.extra.split() if holiday != date])
+                        config_entry.save(update_fields=['extra'])
+                        return Response(status=status.HTTP_204_NO_CONTENT)
+                    else:
+                        return Response(status=status.HTTP_404_NOT_FOUND)
+                
+            else:
+                ret = {
+                    "message": "date not provided or not in the correct format (YYYYMMDD)"
+                }
+
+                return Response(ret, status=status.HTTP_400_BAD_REQUEST)
+
 
     @decorators.action(methods=['GET'], detail=False)
     def statistics(self, request, *args, **kargs):
