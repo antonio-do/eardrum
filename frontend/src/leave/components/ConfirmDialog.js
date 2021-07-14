ab02e7fa56db4098dd0498cfc4c0a1911cda1e71
PhongTran98 <giaphong8124@gmail.com>
2021-07-14 14:38:55 +0800

    Issue #35: Use table to render application detail in confirmation dialog

--- frontend/src/leave/components/ConfirmDialog.js
+++ frontend/src/leave/components/ConfirmDialog.js
@@ -1,17 +1,25 @@
 import React from 'react';
-import { Dialog, DialogContent, DialogContentText, DialogActions, Button, DialogTitle } from '@material-ui/core';
+import { Dialog, DialogContent, DialogActions, Button, DialogTitle, Table, TableRow, TableCell } from '@material-ui/core';
 
 export default ({ content, onConfirm, open, setOpen, title }) => {
     let item = {}
     Object.entries(content).forEach(([key, value]) => {
-        if (key === 'id' || key === 'note') return;
-        if (key === 'is_half') {
-            item['Half day leave on the first day'] = value[0] === 0 ? "No" : "Yes"
-            item['Half day leave on the last day'] = value[1] === 0 ? "No" : "Yes"
-        }
-        else {
-            let capitalizedKey = key.charAt(0).toUpperCase() + key.slice(1);
-            item[capitalizedKey.replace('_', ' ')] = value;
+        switch(key) {
+            case 'id':
+                break;
+            case 'half':
+                item['Half day leave on the first day'] = value[0] === 0 ? "No" : "Yes"
+                item['Half day leave on the last day'] = value[1] === 0 ? "No" : "Yes"
+                break;
+            case 'startdate':
+                item['Start date'] = value
+                break;
+            case 'enddate':
+                item['End date'] = value;
+                break;
+            default:
+                let capitalizedKey = key.charAt(0).toUpperCase() + key.slice(1);
+                item[capitalizedKey.replace('_', ' ')] = value;
         }
     })
     return (
@@ -22,12 +30,19 @@ export default ({ content, onConfirm, open, setOpen, title }) => {
         >
             <DialogTitle>{title}</DialogTitle>
                 <DialogContent>
+                    <Table>
                     {/*TODO: use table*/}
-                    {Object.entries(item).map(([key, value]) => 
-                        (<DialogContentText id="alert-dialog-description">
-                            {key}: {value}
-                        </DialogContentText>)
-                    )}
+                        {Object.entries(item).map(([key, value]) => 
+                            <TableRow>
+                                <TableCell>
+                                    {key}
+                                </TableCell>
+                                <TableCell>
+                                    {value.split('\n').map(line => <div>{line}</div>)}
+                                </TableCell>
+                            </TableRow>
+                        )}
+                    </Table>
                 </DialogContent>
                 <DialogActions>
                     <Button onClick={ () => {onConfirm(); setOpen(false); }} color="primary">
