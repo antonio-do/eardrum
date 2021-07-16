01bca4a43ade48bdb99623944e3074c2396b71ad
PhongTran98 <giaphong8124@gmail.com>
2021-07-16 14:32:52 +0800

    Issue #35: Small fix

--- frontend/src/leave/helpers.js
+++ frontend/src/leave/helpers.js
@@ -15,7 +15,7 @@ const holidayComparator = (holiday1, holiday2) => {
     let now = moment().startOf('day')
     let dif1 = moment(holiday1.date).diff(now, 'days');
     let dif2 = moment(holiday2.date).diff(now, 'days');
-    if (holiday1.date === holiday2.date) return 0;
+    if (holiday1.date.getTime() === holiday2.date.getTime()) return 0;
     // if today is between holiday1 and holiday2
     if (dif1 < 0 ^ dif2 < 0) return dif1 < dif2 ? 1 : -1;
     // if today is either sooner or later than both holiday1 and holiday2
