9fa6d747a870df92fe5ea750d5a43bb24c69ecff
PhongTran98 <giaphong8124@gmail.com>
2021-07-19 11:21:45 +0800

    Issue #36: Use chips in confirmation detail

--- frontend/src/leave/components/ConfirmDialog.js
+++ frontend/src/leave/components/ConfirmDialog.js
@@ -1,26 +1,20 @@
 import React from 'react';
-import { Dialog, DialogContent, DialogActions, Button, DialogTitle, Table, TableRow, TableCell } from '@material-ui/core';
+import { Dialog, DialogContent, DialogActions, Button, DialogTitle, Table, TableRow, TableCell, Chip } from '@material-ui/core';
+import { STATUS_TYPES } from '../constants';
 
 export default ({ content, onConfirm, open, setOpen, title }) => {
-    let item = {}
-    Object.entries(content).forEach(([key, value]) => {
-        switch(key) {
-            case 'id':
-                break;
-            case 'half':
-                item['Half day leave on the first day'] = value[0] === 0 ? "No" : "Yes"
-                item['Half day leave on the last day'] = value[1] === 0 ? "No" : "Yes"
-                break;
-            case 'startdate':
-                item['Start date'] = value
-                break;
-            case 'enddate':
-                item['End date'] = value;
-                break;
-            default:
-                let capitalizedKey = key.charAt(0).toUpperCase() + key.slice(1);
-                item[capitalizedKey.replace('_', ' ')] = value;
-        }
+    let item = JSON.stringify(content) === "{}" ? {} : ({
+        'User': content['user'],
+        'Half day leave on the first day': content['half'][0] === "0" ? "No" : "Yes",
+        'Half day leave on the last day': content['half'][1] === "0" ? "No" : "Yes",
+        'Start date': content['startdate'],
+        'End date': content['enddate'],
+        'Type': <Chip label={content['type']} variant="outlined"/>,
+        'Status': <Chip 
+                    label={content['status']} 
+                    color={content['status'] === STATUS_TYPES.PENDING ? "default" : "primary"}
+                    variant={content['status'] === STATUS_TYPES.REJECTED ? "outlined" : "default"}/>,
+        'Note': content['note'].split('\n').map(line => <div>{line}</div>),
     })
     return (
         <Dialog
@@ -38,7 +32,7 @@ export default ({ content, onConfirm, open, setOpen, title }) => {
                                     {key}
                                 </TableCell>
                                 <TableCell>
-                                    {value.split('\n').map(line => <div>{line}</div>)}
+                                    {value}
                                 </TableCell>
                             </TableRow>
                         )}
